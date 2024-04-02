b4f09c001c6a58d126dd9629f970d3a658f20d17
esturarivitor <162318166+esturarivitor@users.noreply.github.com>
2024-04-02 08:48:44 -0300

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,8 +1,1 @@
-def soma(a,b)
-   return a+b
 
- if _ _name_ _ =="_ _main_ _".
-num1= float(inpurt("digite o primeiro numero:"))
-num2 =float (input(digite o segundo numero:"))
-resultado = soma(num1,num2)
-print(f"resultando:{resultado}")
